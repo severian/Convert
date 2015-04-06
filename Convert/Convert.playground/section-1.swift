75fb8ba8de072cf2353e29936e07724a2672288d
James Baird <james@pancake-labs.com>
2015-04-05 22:14:14 -0700

    Numeric parsers

--- Convert/Convert.playground/section-1.swift
+++ Convert/Convert.playground/section-1.swift
@@ -48,6 +48,30 @@ func >>><A,B>(parser: Parser<A>, f: A -> Parser<B>) -> Parser<B> {
   return parser.flatMap(f)
 }
 
+func run<A>(parser: Parser<A>, input: String) -> A? {
+  return parser.parse(InputState(input: input, pos: 0))?.val
+}
+
+func always<A>(val: A) -> Parser<A> {
+  return Parser<A>() { state in
+    return Result(state: state, val: val)
+  }
+}
+
+func never<A>() -> Parser<A> {
+  return Parser<A>() { state in
+    return nil
+  }
+}
+
+func maybe<A>(val: A?) -> Parser<A> {
+  if let v = val {
+    return always(v)
+  } else {
+    return never()
+  }
+}
+
 func char(c: Character) -> Parser<Character> {
   return Parser<Character>() { state in
     if c == state.first() {
@@ -56,6 +80,17 @@ func char(c: Character) -> Parser<Character> {
     } else {
       return nil;
     }
+  }
+}
+
+func choice<A>(parsers: Parser<A>...) -> Parser<A> {
+  return Parser<A>() { state in
+    for parser in parsers {
+      if let r = parser.parse(state) {
+        return r
+      }
+    }
+    return nil
   }
 }
 
@@ -104,25 +139,54 @@ func digit() -> Parser<Character> {
   return charSet(NSCharacterSet.decimalDigitCharacterSet())
 }
 
-func always<A>(val: A) -> Parser<A> {
-  return Parser<A>() { state in
-    return Result(state: state, val: val)
+func positiveInteger() -> Parser<Int> {
+  return many1(digit()) >>> { digits in
+    return always(String(digits).toInt()!)
   }
 }
 
-func choice<A>(parsers: Parser<A>...) -> Parser<A> {
-  return Parser<A>() { state in
-    for parser in parsers {
-      if let r = parser.parse(state) {
-        return r
-      }
+func negativeInteger() -> Parser<Int> {
+  return char("-") >>> { _ in
+  return positiveInteger() >>> { i in
+    return always(i * -1)
+  }}
+}
+
+func integer() -> Parser<Int> {
+  return choice(positiveInteger(), negativeInteger())
+}
+
+run(negativeInteger(), "-50")
+
+func fraction() -> Parser<Double> {
+  return integer() >>> { numerator in
+  return char("/") >>> { _ in
+  return integer() >>> { denominator in
+    if (denominator != 0) {
+      return always(Double(numerator) / Double(denominator))
+    } else {
+      return never()
     }
-    return nil
-  }
+  }}}
 }
 
-func run<A>(parser: Parser<A>, input: String) -> A? {
-  return parser.parse(InputState(input: input, pos: 0))?.val
+run(fraction(), "1/3")
+
+func decimal() -> Parser<Double> {
+  let dec: Parser<Double> =
+    choice(integer(), always(0)) >>> { intDigits in
+    return char(".") >>> { _ in
+    return positiveInteger() >>> { decDigits in
+      return always(("\(intDigits).\(decDigits)" as NSString).doubleValue)
+    }}}
+  
+  return choice(dec, integer() >>> { n in return always(Double(n)) })
+}
+
+run(decimal(), "5.67")
+
+func number() -> Parser<Double> {
+  return choice(decimal(), fraction())
 }
 
 let a = charSet(NSCharacterSet.uppercaseLetterCharacterSet())
